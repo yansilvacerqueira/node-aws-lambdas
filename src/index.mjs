3aef8ed09e3fac5c8d2f7c74955186e1dfc51ba2
Yan Cerqueira <yansilvacerqueira@outlook.com>
2025-01-04 21:24:34 -0300

    feat: basic example to access a s3 bucket

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,5 @@
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3"
+
 // the function need be async
 export const listUsers = async (event) => {
   const response = JSON.stringify({
@@ -12,4 +14,34 @@ export const listUsers = async (event) => {
   })
 
   return response;
+}
+
+/**
+  that is a basic example to read something of a bucket in s3 aws.
+  keep a attention to not send the package with sdk aws because on aws we have access to skd of aws.
+  remenber of craete a IAM police to allow access to bucket.
+  */
+export const readFileOfBucket = async (event) => {
+  const [record] = event.Records;
+
+  const bucketName = record.s3.bucket.name;
+  const pathFile = record.s3.object.key;
+
+  const s3 = new S3Client();
+  const command = new GetObjectCommand({
+    Bucket: bucketName,
+    Key: pathFile,
+  })
+
+  const { Body } = await s3.send(command);
+
+  const chunks = [];
+  for await (const chunk of Body) {
+    chunks.push(chunk)
+  }
+
+  const buffer = Buffer.concat(chunks).toString("utf8");
+  const users = JSON.parse(buffer)
+
+  return users;
 }
